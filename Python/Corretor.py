4900946889a4a803e475012f9d2978f09d73902c
Thales Menezes <thalesmenezes13@gmail.com>
2019-11-08 12:29:37 -0300

    Atualização código correção

--- Python/Corretor.py
+++ Python/Corretor.py
@@ -1,44 +1,60 @@
-def fail_message(case,output,expected):
-    return "\
-\n\t[TESTE FALHOU]\
-\n\tEntrada  : {}\
-\n\tSaída    : {}".format(repr(case),repr(output)) + \
-("\n\tEsperava : {}".format(repr(expected)) if expected else '')
-
-def runtests(regexp,tests,results=None):
+def runtests(tests,regexp,results=None):
+    '''
+        Executa uma sequência de testes, comparando o
+        resultado de acordo com uma expressão regular
+        ou com uma sequência de outputs experados.
+        
+        Argumentos:
+            testes:  Sequência de inputs de teste
+            regexp:  Expressão regular para conferir resultado
+            results: Sequência de outputs esperados, se informado
+    '''
     results = results or [None]*len(tests)
-    from io import StringIO
     for case,expected in zip(tests,results):
-        # Preparing Buffers:
+        # Preparing Standard Buffers (Input & Output)
+        # Modificando Buffers de Entrada e Saída
+        from io import StringIO
         import sys
-        # Input
-        sys.stdin = StringIO(case)
-        # Output
-        output = StringIO()
-        _backup,sys.stdout = sys.stdout, output
-        del sys
+        sys.stdin, output = StringIO(case), StringIO()
+        _backup, sys.stdout = sys.stdout, output
+        del sys, StringIO
         # Run code
+        # Executando código
         try:
             main()
         except NameError:
-            raise NameError("\n\tFunção `main` não\
-            encontrada:\n\t\tEscreveste o código no local\
-            correto?\n\t\t`def main():\n\t\t\t# Escreva aqui a\
-            solução`")
-        # Collect output
+            raise NameError("\n\tFunção `main` não encontrada:\n" +\
+            "\t\tEscreveste o código no local correto?\n" +\
+            "\t\t`def main():\n" +\
+            "\t\t\t# Escreva aqui a solução`")
+        # Collect output state
+        # Coletando Status Buffer Saída
         output = output.getvalue()
         # Reset standart output
+        # Restaurando Buffer Saída
         import sys
         sys.stdout = _backup
         del sys
-        # Gives the user current Test output
+        # Prompt test status
+        # Imprime o status do test
         print('>>> Input:\n', case, sep='')
         print('>>> Output:')
         print(output,end="\n"*2)
-        # Check if output is correct
-        output = output.rstrip('\n') #Removing trailing \n
+        # Validate output
+        # Valida a saída
+        output = output.rstrip('\n')
+        # Remove trailing \n
+        # Remove '\n's ao final da linha
         from re import search as find
-        if expected and expected != output or\
-                not find(regexp.format(*case.split('\n')),output):
+        if expected and expected != output or not find(
+           regexp.format(*case.split('\n')),output
+        ):
+            # Fail message template
+            # Template de mensagem de falha
+            fail_message = lambda case,output,expected : '''
+    [TESTE FALHOU]
+    Entrada  : {}
+    Saída    : {}'''.format(repr(case),repr(output)) + ('''
+    Esperava : {}'''.format(repr(expected)) if expected else '')
             raise ValueError(fail_message(case,output,expected))
         del find
