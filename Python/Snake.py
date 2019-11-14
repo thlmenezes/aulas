f6255319fae06bbb1a0a14e78f92d9260d52ad32
Thales Menezes <thalesmenezes13@gmail.com>
2019-11-14 11:05:13 -0300

    new/Python/Snake.py
    Implementada movimentação para fins de motivação da atividade

--- Python/Snake.py
+++ Python/Snake.py
@@ -1,11 +1,52 @@
-tela = [  list(10*' ') for _ in range(10) ]
+'''
+Modificações:
+
+    ALTURA e LARGURA
+    troca imprimir     para matriz_repr
+    troca imprimirTela para geraTela
+    def movimenta
+    uso de lista de coordenadas
+'''
+''' COPY PASTE 14.11.19
+def matriz_repr(matriz:list): return ''.join([ ''.join(linha)+'\n' for linha in matriz ])
+def geraTela(tela:list):\
+    return matriz_repr([list('.'+LARGURA*'_'+'.')] + \
+                       [ ['|'] + linha + ['|'] for linha in tela] + \
+                       [list('|'+LARGURA*'_'+'|')])
+
+# Inicialização da tela e cobra
+ALTURA = LARGURA = 10
+tela = [  list(LARGURA*' ') for _ in range(ALTURA) ]
+cobra = [(LARGURA//2,ALTURA//2)]
+tela[ALTURA//2][LARGURA//2] = 'o'
+
+# Menu inicial básico
+input('Aperte Enter')
+
+# Tela de jogo
+print("Para sair pressione ESC",geraTela,sep='\n',end='')
+
+# Gameloop
+from ioCustom import kbhit,getch,clear
+# TODO: pygame
+while True:
+    clear()
+    if kbhit():
+        move = getch()
+    print("Para sair pressione ESC",geraTela,sep='\n',end='')
+'''
+ALTURA = LARGURA = 10
+tela = [  list(LARGURA*' ') for _ in range(ALTURA) ]
 # Matriz tela
 # NOTE: Resolução será 320*240
 # Ficou confuso como criei essa lista?
-#     Observe o próximo comentário
-
-imprimir = lambda tela : print( *[ ''.join(linha)+'\n' for linha in tela ], sep='',end='')
+#     Observe os próximos comentários
+def matriz_repr(matriz:list): return ''.join([ ''.join(linha)+'\n' for linha in matriz ])
+''' matriz_repr -> Gera uma representação de uma matriz qualquer '''
 '''
+[DEPRECATED] imprimir
+imprimir = lambda tela : print( *[ ''.join(linha)+'\n' for linha in tela ], sep='',end='')
+
 Definindo uma função em uma única linha,
 utilizando definição anônima de função;
 equivalente à:
@@ -38,9 +79,16 @@ Observações:
         Caractere Nova Linha (Newline), responsável por "descer uma linha"
         leia sobre em https://pt.wikipedia.org/wiki/Nova_linha
 '''
-cobra_x = cobra_y = 0
-tela[cobra_x][cobra_y] = "o"
+cobra = [(LARGURA//2,ALTURA//2)]
+tela[ALTURA//2][LARGURA//2] = 'o'
 '''
+A cobrinha será representando como um
+encadeamento de suas "partes", conforme
+ela cresce ela vai ocupando mais espaço
+na tela; armazenamos uma lista de todos
+os pontos, com as coordenadas X e Y, que
+ela ocupa na tela
+
 A cobrinha tem, no inicio, um corpo de
 tamanho 1 no canto superior esquerdo
 da tela, representado por 'o'
@@ -51,31 +99,41 @@ tela[0] corresponde à primeira linha da tela
 que por sua vez é uma lista, portanto permite
 acesso posicional usando []; logo tela[0][0]
 representa o 1º elemento da 1ª linha.
+tela[ALTURA//2][LARGURA//2] é +- meio da tela.
 '''
-
 input('Aperte Enter')
 # Menu inicial básico
-
-imprimirTela = lambda tela : imprimir([list('.'+10*'_'+'.')] + [ ['|'] + linha + ['|'] for linha in tela] + [list('|'+10*'_'+'|')])
-imprimirTela(tela)
+def geraTela(tela:list):\
+    return matriz_repr([list('.'+LARGURA*'_'+'.')] + \
+                       [ ['|'] + linha + ['|'] for linha in tela] + \
+                       [list('|'+LARGURA*'_'+'|')])
+print("Para sair pressione ESC",geraTela,sep='\n',end='')
 # Imprime o estado do jogo
-
-from ioCustom import inputchar
-move = inputchar()
-# Movimentação será usando WASD
-if move.lower()=="w":
-    cobra_y -= 1
-
-'''
-cobra = [(0,0)]
-
-A cobrinha será representando como um
-encadeamento de suas "partes", conforme
-ela cresce ela vai ocupando mais espaço
-na tela; armazenamos uma lista de todos
-os pontos, com as coordenadas X e Y, que
-ela ocupa na tela
-'''
+def movimenta(matriz: list, atual: tuple, nova: tuple):\
+    matriz[atual[1]][atual[0]],matriz[nova[1]][nova[0]] = ' ','o'
+# Apaga de uma posição e imprime em outra
+from ioCustom import kbhit,getch,clear
+while True:
+    clear()
+    if kbhit():
+        move = getch()
+        cobra_x,cobra_y = cobra[-1]
+        # Movimentação será usando WASD
+        if move.lower()=="w":
+            cobra_y -= 0 if cobra_y - 1 < 0         else 1
+        elif move.lower()=="s":
+            cobra_y += 0 if cobra_y + 1 > ALTURA-1  else 1
+        elif move.lower()=="a":
+            cobra_x -= 0 if cobra_x - 1 < 0         else 1
+        elif move.lower()=="d":
+            cobra_x += 0 if cobra_x + 1 > LARGURA-1 else 1
+        elif move == chr(27):# == '\x1b' == escape
+            break
+        # mover em tela de ATUAL para NOVA
+        movimenta(tela,cobra[-1],(cobra_x,cobra_y))
+        cobra.pop()
+        cobra.append((cobra_x,cobra_y))
+    print("Para sair pressione ESC",geraTela,sep='\n',end='')
 '''
 TODOs:
 - Movimentação
--- Python/ioCustom.py
+++ Python/ioCustom.py
@@ -1,42 +1,90 @@
 # https://github.com/magmax/python-readchar
 class _Getch:
-    '''Gets a single character from standard input.
-       Does not echo to the screen.
-       DOESN'T WORK IN IDLE
+    '''Captura um caractere do Input Padrão,
+       sem ecoar na tela.
+       NOTA: Gera interrupção da execução.
+       NÃO FUNCIONA NO IDLE
     '''
     def __init__(self):
         import sys
         if sys.platform.startswith('linux'):
-            self.impl = _GetchUnix()
+            def getch():
+                import tty, sys, termios
+                fd = sys.stdin.fileno()
+                old_settings = termios.tcgetattr(fd)
+                try:
+                    tty.setraw(sys.stdin.fileno())
+                    ch = sys.stdin.read(1)
+                finally:
+                    termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+                return ch
         elif sys.platform in ('win32', 'cygwin'):
-            self.impl = _GetchWindows()
+            from msvcrt import getch
         else:
-            raise NotImplemented('The platform {} is not supported yet'.format(sys.platform))
-        del sys
+            raise TypeError('NotImplemented: The platform {} is not supported yet'.format(sys.platform))
+        self.impl = getch
 
     def __call__(self): return self.impl()
 
-class _GetchUnix:
-    def __init__(self): return
+class _Kbhit:
+    '''True se o teclado foi clicado, Falso caso contrário.
+       True if keyboard was hit, False otherwise.
+       NÃO FUNCIONA NO IDLE
+    '''
+    def __init__(self):
+        import sys
+        if sys.platform.startswith('linux'):
+            import termios, atexit
+            fd = sys.stdin.fileno()
+            # save the terminal settings
+            old_settings = termios.tcgetattr(fd)
+            # switch to normal terminal when finished
+            atexit.register(lambda : termios.tcsetattr(fd, termios.TCSAFLUSH, old_settings))
+            new_term  = termios.tcgetattr(fd)
+            # new terminal setting unbuffered
+            new_term[3] = (new_term[3] & ~termios.ICANON & ~termios.ECHO)
+            # switch to unbuffered terminal
+            termios.tcsetattr(fd, termios.TCSAFLUSH, new_term)
+            def kbhit():
+                import sys
+                from select import select
+                return select([sys.stdin], [], [], 0)[0] != []
+        elif sys.platform in ('win32', 'cygwin'):
+            from msvcrt import kbhit
+        else:
+            raise TypeError('NotImplemented: The platform {} is not supported yet'.format(sys.platform))
+        self.impl = kbhit
 
-    def __call__(self):
-        import tty, sys, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
+    def __call__(self): return self.impl()
 
+def clear():
+    '''
+        Limpa a tela do terminal
+    '''
+    import os
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #print(chr(27) + '[2J')
 
-class _GetchWindows:
-    def __init__(self): return
+class Up:
+    '''
+        Ajuda na limpeza da tela do IDLE
+    '''
+    def __init__(self, size = None):
+        self.size = size or 21
+    def __call__(self, size = 0):
+        print('\n'*(size or self.size),end='')
+    def __repr__(self):
+        return '\n'*self.size
 
-    def __call__(self):
-        import msvcrt
-        return msvcrt.getch()
+def getch():
+    '''
+        Captura um caractere do Input Padrão,
+        NOTA: caso já exista buffer,
+              não gera interrupção.
+    '''
+    import sys
+    return sys.stdin.read(1)
 
-
+up = Up()
 inputchar = _Getch()
+kbhit = _Kbhit()
