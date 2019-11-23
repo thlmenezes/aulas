cb613cd788b3061808bbf580de5d79545bfcc98d
Thales Menezes <thalesmenezes13@gmail.com>
2019-11-23 16:53:25 -0300

    refactoring/Python
    - [ ICC ] Refatoração do código 7.9

--- Python/[ ICC ] Lista 7 Questão 9 - Seleção de Atividades.py
+++ Python/[ ICC ] Lista 7 Questão 9 - Seleção de Atividades.py
@@ -1,32 +1,30 @@
-def time(texto:str):
+def time(tempoEmTexto:str):
     '''
-        Converte representação HH:MM para um único inteiro
-        em segundos
+        Converte representação HH:MM
+        para um único inteiro SS
     '''
-    index = texto.find(':')
-    return int(texto[:index])*60+int(texto[index+1:])
+    horas,minutos = tempoEmTexto.split(':')
+    return int(horas)*60+int(minutos)
 
 # Recebemos todas as tarefas possíveis para o dia; e
-tarefas = {}
+afazeres = {}
 for _ in range(int(input())):
     *nome,inicio,fim = input().split()
-    tarefas[' '.join(nome)] = {'inicio':time(inicio),'fim':time(fim)}
+    afazeres[' '.join(nome)] = \
+    {'inicio':time(inicio),'fim':time(fim)}
 
-inicial = sorted(tarefas,key=lambda chave: tarefas[chave]['fim'])
+diaIdeal= []## lista de atividades do dia
+fimDia  = 0 ## horário de encerramento do dia
 # Ordenamos as tarefas por seus horários de término.
-if inicial:
-    # O dia ideal começa com a tarefa que acaba mais cedo; e
-    ideal   = [inicial[0]]
-    last    = inicial[0]
-    # para todas as atividades restantes:
-    for atividade in inicial[1:]:
-        # Avaliamos se a atividade começa depois da última ter terminado;
-        if tarefas[atividade]['inicio'] >= tarefas[last]['fim']:
-            # Caso aconteça: inserimos ao nosso dia ideal e
-            ideal.append(atividade)
-            last = atividade
-            # atualizamos o nome da última atividade.
-    print(len(ideal))
-    print(*ideal,sep='\n')
-else:
-    print(0)
+for atividade in sorted(afazeres,\
+                 key=lambda chave: tarefas[chave]['fim']):
+    # Para toda atividade:
+    #     Avaliamos se começa depois da última atividade
+    #     do dia ter terminado;
+    if tarefas[atividade]['inicio'] >= fimDia:
+        # Caso aconteça:
+        #     inserimos ao nosso dia ideal e
+        diaIdeal.append(atividade)
+        last = afazeres[atividade]['fim']
+        #     atualizamos o fim do dia.
+print(len(diaIdeal),*diaIdeal,sep='\n')
