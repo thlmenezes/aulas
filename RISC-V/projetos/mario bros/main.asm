0677c99c38ac37f729eec14226796b4ef19454a4
Thales Menezes <thlmenezes@pm.me>
2020-11-15 22:03:20 -0300

    feat(RISCV): example FILEIO usage

--- RISC-V/projetos/mario bros/main.asm
+++ RISC-V/projetos/mario bros/main.asm
@@ -21,19 +21,21 @@
 .include "./KMMIO_MACROS.s"
 .include "./STDIO.s"
 .include "./BMPDISPLAY.s"
+.include "./FILEIO.s"
 # ============================================================
 #                           MAIN
 # ============================================================
 .text
 main:
-	li a0, BMP_F0_BEGIN
-	li a1, 0x55    # COR
-	li a2, 40      # Comprimento
-	li a3, 40      # Altura
-	jal BMP_draw_square
-	Exit()
-
-
-
-
-
+	# Abrir Arquivo
+	FILE_OPEN(FILE.READ,"./mario bros/assets/original/menu.bin")
+	bltz a0, exit  # if a0 < 0 -> ERROR
+	mv s0, a0      # backup do file descriptor
+	
+	# Ler Arquivo
+	FILE_READ(s0,BMP_F0_BEGIN,0x12C00) # 320x240
+	syscall(INT)  # print int -> nº de leituras bem sucedidas
+	
+	# Fecha o arquivo
+	FILE_CLOSE(s0)
+exit:	syscall(EXIT2)
